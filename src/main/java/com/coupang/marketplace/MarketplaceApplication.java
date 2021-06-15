3617eb6c16a53ab8f1861b57de3abda2a66f0097
sieun <31584255+sileeee@users.noreply.github.com>
2021-06-16 03:03:43 +0900

    [#1] 프로젝트 생성 (#5)
    * chore: project init
    
    * chore: thymeleaf 의존성 제거

---
+++ src/main/java/com/coupang/marketplace/MarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.coupang.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceApplication.class, args);
+    }
+
+}
