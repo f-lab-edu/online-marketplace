3eff7e76c53475e4fa215741b3633355dc7a13a5
sileeee <tldms201@naver.com>
2021-06-22 05:16:14 +0900

    feat: 로그인 핸들러 추가

--- src/main/java/com/coupang/marketplace/controller/UserController.java
+++ src/main/java/com/coupang/marketplace/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.coupang.marketplace.controller;
 
+import com.coupang.marketplace.domain.User;
 import com.coupang.marketplace.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,6 +16,15 @@ public class UserController {
     @PostMapping("/users/sign-up")
     public String signUp(@Valid @RequestBody SignUpRequestDto requestDto) {
         userService.save(requestDto);
+        return "success";
+    }
+
+    @PostMapping("/users/login")
+    /*@ResponseStatus(HttpStatus.OK)*/
+    public String signIn(@Valid @RequestBody SignInRequestDto requestDto) {
+        Optional<User> user = userService.login(requestDto);
+        
+        //TO DO : 리턴값에 따라 다른 처리
         return "success";
     }
 }
--- src/main/java/com/coupang/marketplace/service/UserService.java
+++ src/main/java/com/coupang/marketplace/service/UserService.java
@@ -1,10 +1,13 @@
 package com.coupang.marketplace.service;
 
+import com.coupang.marketplace.controller.SignInRequestDto;
 import com.coupang.marketplace.controller.SignUpRequestDto;
 import com.coupang.marketplace.domain.User;
 import com.coupang.marketplace.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -25,5 +28,24 @@ public class UserService {
 
     public boolean checkIfUserExist (String email) {
         return userRepository.findByEmail(email) != null ? true : false;
+    }
+
+    public Optional<User> login(SignInRequestDto dto){
+        if (!checkIfUserExist(dto.getEmail())) {
+            throw new Error("존재하지 않는 이메일입니다.");
+        }
+        else {
+            Optional<User> user = userRepository.findByEmail(dto.getEmail());
+
+            String email = dto.getEmail();
+            String password = dto.getPassword();
+            
+            //TO DO : 암호화된 비밀번호와 일치하는지 확인
+
+            //TO DO : 실패
+
+            //성공
+            return user;
+        }
     }
 }
