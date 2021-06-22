2368477486900ed48931a2b8e6213b328b98059b
hello-jiwon <jiwon.developer@gmail.com>
2021-06-22 23:37:43 +0900

    refactor: spring security 없이 비밀번호 암호화

--- src/main/java/com/coupang/marketplace/util/CryptoUtil.java
+++ src/main/java/com/coupang/marketplace/util/CryptoUtil.java
@@ -1,13 +1,52 @@
 package com.coupang.marketplace.util;
 
-import org.springframework.security.crypto.bcrypt.BCrypt;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
 
 public class CryptoUtil {
-    public static String generateSalt() {
-        return BCrypt.gensalt(12);
+    public static String generateSalt(){
+        Random random = new Random();
+
+        byte[] salt = new byte[8];
+        random.nextBytes(salt);
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < salt.length; i++) {
+            sb.append(String.format("%02x",salt[i]));
+        }
+
+        return sb.toString();
+
     }
 
     public static String encryptPassword(String plainPassword, String salt) {
-        return BCrypt.hashpw(plainPassword, salt);
+        String result = "";
+
+        byte[] byteSalt = salt.getBytes();
+        byte[] a = plainPassword.getBytes();
+        byte[] bytes = new byte[a.length + byteSalt.length];
+
+        System.arraycopy(a, 0, bytes, 0, a.length);
+        System.arraycopy(byteSalt, 0, bytes, a.length, byteSalt.length);
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(bytes);
+
+            byte[] byteData = md.digest();
+
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xFF) + 256, 16).substring(1));
+            }
+
+            result = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
