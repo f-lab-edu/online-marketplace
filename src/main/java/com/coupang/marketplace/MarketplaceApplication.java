5a73dd6794a7d3eaf67ee4a55c764b95ae2920f4
hello-jiwon <jiwon.developer@gmail.com>
2021-07-16 13:40:18 +0900

    feat: redis 연동하기

--- src/main/java/com/coupang/marketplace/MarketplaceApplication.java
+++ src/main/java/com/coupang/marketplace/MarketplaceApplication.java
@@ -2,7 +2,9 @@ package com.coupang.marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @SpringBootApplication
 public class MarketplaceApplication {
 
