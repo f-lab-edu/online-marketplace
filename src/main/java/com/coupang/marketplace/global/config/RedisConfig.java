220f6c4cc68349da3b3ce914868310fb14d71628
sileeee <tldms201@naver.com>
2021-07-21 20:49:07 +0900

    refactor: 불필요한 import 삭제

--- src/main/java/com/coupang/marketplace/global/config/RedisConfig.java
+++ src/main/java/com/coupang/marketplace/global/config/RedisConfig.java
@@ -6,9 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import com.coupang.marketplace.global.constant.SessionKey;
