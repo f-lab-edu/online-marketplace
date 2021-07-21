19d8196921e5a658e01bb7a5d01d6c99fbd38758
hello-jiwon <jiwon.developer@gmail.com>
2021-07-22 05:45:55 +0900

    feat: 컨트롤러 테스트 케이스 추가

--- src/test/java/com/coupang/marketplace/product/controller/ProductControllerTest.java
+++ src/test/java/com/coupang/marketplace/product/controller/ProductControllerTest.java
@@ -3,23 +3,18 @@ package com.coupang.marketplace.product.controller;
 
 import com.coupang.marketplace.global.util.MultiValueMapConverter;
 import com.coupang.marketplace.product.controller.dto.GetProductsRequest;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.MultiValueMap;
 
 import static com.coupang.marketplace.product.constant.DeliveryTypeEnum.ROCKET;
+import static com.coupang.marketplace.product.constant.DeliveryTypeEnum.ROCKET_FRESH;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class ProductControllerTest extends ControllerTest {
-
-    @BeforeAll
-    public void setUp(){
-
-    }
 
     @DisplayName("로켓 배송을 선택하면 로켓 배송 가능 상품 목록을 보여준다.")
     @Test
@@ -35,7 +30,30 @@ public class ProductControllerTest extends ControllerTest {
         // when
         final ResultActions actions = mvc.perform(get("/products")
             .params(params))
-            .andDo(print());;
+            .andDo(print());
+
+        // then
+        actions
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @DisplayName("로켓 프레쉬 배송을 선택하고 로켓 배송 필터링을 적용하면 로켓 프레쉬와 로켓 배송 모두 가능한 상품 전체를 가져온다.")
+    @Test
+    public void getProductsByIsRocketAndIsRocketFresh() throws Exception {
+        // given
+        final GetProductsRequest dto = GetProductsRequest.builder()
+                .deliveryType(ROCKET_FRESH)
+                .isRocket(true)
+                .page(1)
+                .listSize(2)
+                .build();
+        final MultiValueMap<String, String> params = MultiValueMapConverter.convert(objectMapper, dto);
+
+        // when
+        final ResultActions actions = mvc.perform(get("/products")
+                .params(params))
+                .andDo(print());
 
         // then
         actions
