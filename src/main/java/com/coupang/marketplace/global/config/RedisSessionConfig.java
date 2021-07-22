d942705d677b19d5556b5d1f7d6e7029531ba201
jiwon <79824919+hello-jiwon@users.noreply.github.com>
2021-07-22 12:51:47 +0900

    [#12] 세션 스토리지로 redis 연동하기 (#21)
    * feat: redis 연동하기
    
    * refactor: config명 명확하게 변경
    
    * fix: 불필요한 RedisTemplate 삭제

---
+++ src/main/java/com/coupang/marketplace/global/config/RedisSessionConfig.java
@@ -0,0 +1,25 @@
+package com.coupang.marketplace.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+@Configuration
+public class RedisSessionConfig {
+
+    @Value("${spring.redis.session.host}")
+    private String host;
+
+    @Value("${spring.redis.session.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
+}
