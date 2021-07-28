30359e774e977d7045467fc62b1189b7adbeb2f6
sileeee <tldms201@naver.com>
2021-07-29 00:41:37 +0900

    fix: Timestamp형식으로 변경

--- src/main/java/com/coupang/marketplace/coupon/domain/Coupon.java
+++ src/main/java/com/coupang/marketplace/coupon/domain/Coupon.java
@@ -1,7 +1,7 @@
 package com.coupang.marketplace.coupon.domain;
 
 import java.math.BigInteger;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -21,11 +21,11 @@ public class Coupon {
 
 	private BigInteger productId;
 
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	private LocalDateTime expirationTime;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private Timestamp expirationTime;
 
 	@Builder
-	public Coupon(BigInteger id, String name, BigInteger minPrice, BigInteger discountPrice, BigInteger productId, LocalDateTime expirationTime){
+	public Coupon(BigInteger id, String name, BigInteger minPrice, BigInteger discountPrice, BigInteger productId, Timestamp expirationTime){
 		this.id = id;
 		this.name = name;
 		this.minPrice = minPrice;
