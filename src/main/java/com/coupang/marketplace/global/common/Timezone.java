50a86967b585ab8c93bf2349f44793c07892f83b
sileeee <tldms201@naver.com>
2021-08-06 19:45:19 +0900

    refactor: 의미 없는 코드 삭제

--- src/main/java/com/coupang/marketplace/global/common/Timezone.java
+++ src/main/java/com/coupang/marketplace/global/common/Timezone.java
@@ -1,15 +1,7 @@
 package com.coupang.marketplace.global.common;
 
-import java.util.TimeZone;
-
-import javax.annotation.PostConstruct;
 
 public class Timezone {
 
 	public static final String LocalTimezone = "UTC";
-
-	@PostConstruct
-	public void setTimezone() {
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-	}
 }
