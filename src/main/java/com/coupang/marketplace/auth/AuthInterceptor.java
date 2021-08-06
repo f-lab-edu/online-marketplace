279e2dcabe5c1021a6df72b166153126b059947f
hello-jiwon <jiwon.developer@gmail.com>
2021-08-07 06:54:43 +0900

    fix: null 처리

--- src/main/java/com/coupang/marketplace/auth/AuthInterceptor.java
+++ src/main/java/com/coupang/marketplace/auth/AuthInterceptor.java
@@ -1,5 +1,6 @@
 package com.coupang.marketplace.auth;
 
+import com.coupang.marketplace.auth.exception.NoAuthorizationData;
 import com.coupang.marketplace.auth.exception.UnauthorizedException;
 import com.coupang.marketplace.global.constant.SessionKey;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.Optional;
 
 
 @Component
@@ -43,12 +45,14 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     private String getUserIdBySession(HttpServletRequest request){
         HttpSession session = request.getSession();
-        return session.getAttribute(SessionKey.LOGIN_USER_ID)
-                .toString();
+        return Optional.ofNullable(session.getAttribute(SessionKey.LOGIN_USER_ID))
+                .map(v -> v.toString())
+                .orElseThrow(NoAuthorizationData::new);
     }
 
     private String getUserIdByPathVariable(HttpServletRequest request){
         Map<String, String> pathVariables = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
-        return pathVariables.get("id");
+        return Optional.ofNullable(pathVariables.get("id"))
+                .orElseThrow(NoAuthorizationData::new);
     }
 }
