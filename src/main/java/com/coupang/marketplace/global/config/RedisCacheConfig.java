2afd9cad25f51772d3b5414734bb86609808b06c
hello-jiwon <jiwon.developer@gmail.com>
2021-08-12 01:52:27 +0900

    fix: 캐시에 따라 정책 개별 적용

--- src/main/java/com/coupang/marketplace/global/config/RedisCacheConfig.java
+++ src/main/java/com/coupang/marketplace/global/config/RedisCacheConfig.java
@@ -1,19 +1,21 @@
 package com.coupang.marketplace.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
+
 
 @EnableCaching
 @Configuration
@@ -31,18 +33,26 @@ public class RedisCacheConfig {
     }
 
     @Bean
-    public RedisCacheManager redisCacheManager() {
-        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(redisCacheConnectionFactory());
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(redisCacheConnectionFactory());
+        return redisTemplate;
+    }
 
-        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+    @Bean
+    public RedisCacheConfiguration defaultRedisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
                 .disableCachingNullValues()
                 .entryTtl(Duration.ofHours(1L))
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
-
-        builder.cacheDefaults(configuration);
-        return builder.build();
     }
 
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return (builder) -> builder
+                .withCacheConfiguration("products", defaultRedisCacheConfiguration().entryTtl(Duration.ofMinutes(30)));
+    }
 }
