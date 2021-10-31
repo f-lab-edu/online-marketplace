ad6ab13f1702ea221c4aa3a6ba835b71e98012f1
hello-jiwon <jiwon.developer@gmail.com>
2021-10-31 21:01:24 +0900

    feat: 테스트코드 fixture 생성

--- src/test/java/com/coupang/marketplace/global/fixture/CouponFixture.java
+++ src/test/java/com/coupang/marketplace/global/fixture/CouponFixture.java
@@ -13,8 +13,7 @@ public class CouponFixture {
 		public static final Long MIN_PRICE = 30000L;
 		public static final Long DISCOUNT_PRICE = 2000L;
 		public static final Long PRODUCT_ID = 11L;
-		public static final ZonedDateTime EXPIRATION_TIME = ZonedDateTime.of(2022, 12, 01, 20, 30, 0, 0, ZoneId.of("UTC"));
-		public static final Integer MAX_COUPON_COUNT = 5;
+		public static final ZonedDateTime EXPIRATION_TIME = ZonedDateTime.of(2021, 10, 01, 20, 30, 0, 0, ZoneId.of("UTC"));
 
 		public static final Coupon COUPON = Coupon.builder()
 			.id(ID)
@@ -23,47 +22,6 @@ public class CouponFixture {
 			.discountPrice(DISCOUNT_PRICE)
 			.productId(PRODUCT_ID)
 			.expirationTime(EXPIRATION_TIME)
-			.maxCouponCount(MAX_COUPON_COUNT)
-			.build();
-	}
-
-	public static class Coupon2 {
-		public static final Long ID = 2L;
-		public static final String NAME = "coupon2";
-		public static final Long MIN_PRICE = 20000L;
-		public static final Long DISCOUNT_PRICE = 1000L;
-		public static final Long PRODUCT_ID = 11L;
-		public static final ZonedDateTime EXPIRATION_TIME = ZonedDateTime.of(2021, 12, 05, 14, 00, 0, 0, ZoneId.of("UTC"));
-		public static final Integer MAX_COUPON_COUNT = 5;
-
-		public static final Coupon COUPON = Coupon.builder()
-			.id(ID)
-			.name(NAME)
-			.minPrice(MIN_PRICE)
-			.discountPrice(DISCOUNT_PRICE)
-			.productId(PRODUCT_ID)
-			.expirationTime(EXPIRATION_TIME)
-			.maxCouponCount(MAX_COUPON_COUNT)
-			.build();
-	}
-
-	public static class Coupon3 {
-		public static final Long ID = 3L;
-		public static final String NAME = "coupon3";
-		public static final Long MIN_PRICE = 40000L;
-		public static final Long DISCOUNT_PRICE = 5000L;
-		public static final Long PRODUCT_ID = 11L;
-		public static final ZonedDateTime EXPIRATION_TIME = ZonedDateTime.of(2020, 12, 05, 14, 00, 0, 0, ZoneId.of("UTC"));
-		public static final Integer MAX_COUPON_COUNT = 5;
-
-		public static final Coupon COUPON = Coupon.builder()
-			.id(ID)
-			.name(NAME)
-			.minPrice(MIN_PRICE)
-			.discountPrice(DISCOUNT_PRICE)
-			.productId(PRODUCT_ID)
-			.expirationTime(EXPIRATION_TIME)
-			.maxCouponCount(MAX_COUPON_COUNT)
 			.build();
 	}
 }
