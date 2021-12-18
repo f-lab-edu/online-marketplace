fdfd343b0d19184aec0ccd6678cac15f14546f2b
hello-jiwon <jiwon.developer@gmail.com>
2021-12-18 11:34:15 +0900

    fix: 테스트 코드 로그인 확인 부분 수정

--- src/test/java/com/coupang/marketplace/review/controller/ReviewControllerTest.java
+++ src/test/java/com/coupang/marketplace/review/controller/ReviewControllerTest.java
@@ -1,10 +1,13 @@
 package com.coupang.marketplace.review.controller;
 
+import com.coupang.marketplace.global.constant.SessionKey;
 import com.coupang.marketplace.global.fixture.ImageFixture.*;
 import com.coupang.marketplace.global.fixture.ReviewFixture.*;
+import com.coupang.marketplace.global.fixture.UserFixture;
 import com.coupang.marketplace.global.template.ControllerTestTemplate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,12 +26,16 @@ public class ReviewControllerTest extends ControllerTestTemplate {
         final String content = Review1.CONTENT;
         final MultipartFile file = Image1.MULTIPART_FILE;
 
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(SessionKey.LOGIN_USER_ID, UserFixture.User1.ID);
+
         // when
         final ResultActions actions = mvc.perform(multipart("/reviews")
                 .file("img", file.getBytes())
                 .param("productId", String.valueOf(Review1.PRODUCT_ID))
                 .param("score", String.valueOf(Review1.SCORE))
-                .param("content", Review1.CONTENT))
+                .param("content", Review1.CONTENT)
+                .session(session))
                 .andDo(print());
 
         // then
