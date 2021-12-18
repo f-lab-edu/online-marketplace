9c17aecfa93937cc427e1071dc5e3d53f246656c
hello-jiwon <jiwon.developer@gmail.com>
2021-12-18 11:22:07 +0900

    fix: 로그인 확인하는 부분 수정

--- src/main/java/com/coupang/marketplace/review/controller/ReviewController.java
+++ src/main/java/com/coupang/marketplace/review/controller/ReviewController.java
@@ -1,9 +1,12 @@
 package com.coupang.marketplace.review.controller;
 
+import com.coupang.marketplace.auth.AuthRequired;
 import com.coupang.marketplace.global.common.SuccessResponse;
 import com.coupang.marketplace.review.controller.dto.CreateReviewRequest;
 import com.coupang.marketplace.review.service.ReviewService;
+import com.coupang.marketplace.user.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +19,16 @@ import javax.validation.Valid;
 public class ReviewController {
 
     private final ReviewService reviewService;
+    @Qualifier("userSessionLoginService")
+    private final LoginService loginService;
 
+
+
+    @AuthRequired
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public SuccessResponse createReview(@Valid CreateReviewRequest dto){
-        long userId = 1; // 하드코딩
+        Long userId = loginService.getLoginUserId();
         reviewService.createReview(userId, dto);
         return SuccessResponse.builder()
                 .message("리뷰 등록 성공")
